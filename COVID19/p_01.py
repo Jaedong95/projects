f913115cad17c6f1ecea0e987c5894929811fc35
JaeDong_95 <badzoo@naver.com>
2021-11-17 10:23:03 +0900

    .

---
+++ COVID19/p_01.py
@@ -0,0 +1,21 @@
+# 개인 프로젝트 1. 데이터를 주기적으로 크롤링하여 MySQL에 저장하고, 이를 ELK를 통해 분석 
+''' Application Design '''
+
+''' func1. Data Crawling '''
+def get_news(site, keyword):
+    pass
+
+
+''' func2. MySQL '''
+def connect_mysql():
+    pass 
+
+def store_data():
+    pass
+
+def get_data():
+    pass 
+
+''' func3. Auto Crawling  - daily '''
+def daily_crawl():
+    pass 
